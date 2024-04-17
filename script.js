d6eeb830a2b290ef26440224e32e96cb76cc3d84
Marcio Januario <marcio0512@gmail.com>
2024-04-16 22:16:18 -0400

    Fim da página com Java Script Funcionando

---
+++ script.js
@@ -0,0 +1,31 @@
+const dia = document.getElementById('dia')
+const hora = document.getElementById('hora')
+const minuto = document.getElementById('minuto')
+const segundo = document.getElementById('segundo')
+
+const evento = "19 apr 2024"
+
+function countdown() {
+    const dataEvento = new Date(evento)
+    const hoje = new Date()
+
+    const segTotal = (dataEvento - hoje)/1000;
+
+    const finalDias = Math.floor(segTotal / 60 / 60 / 24)
+    const finalHoras = Math.floor(segTotal / 60 / 60) % 24
+    const finalMinutos = Math.floor(segTotal / 60) % 60
+    const finalSegundos = Math.floor(segTotal) % 60
+
+    dia.innerHTML = `${formatoTempo(finalDias)}D`
+    hora.innerHTML = `${formatoTempo(finalHoras)}H`
+    minuto.innerHTML = `${formatoTempo(finalMinutos)}M`
+    segundo.innerHTML = `${formatoTempo(finalSegundos)}S`
+
+function formatoTempo(tempo){
+    return tempo < 10? `0${tempo}` : tempo;
+}
+
+}
+
+countdown();
+setInterval(countdown, 1000)
